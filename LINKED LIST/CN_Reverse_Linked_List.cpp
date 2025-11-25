61671609697d28367f23619c05ca0837d12f12ae
Aniket Vyavhare <146797997+amv2809@users.noreply.github.com>
2025-11-25 17:30:50 +0530

    Added the Coding Ninjas Question Solution for Reversing the Linked List

---
+++ LINKED LIST/CN_Reverse_Linked_List.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    // Write your code here
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    else{
+    LinkedListNode<int>* forward = NULL; 
+    LinkedListNode<int>* previous = NULL; 
+    LinkedListNode<int>* current = head; 
+    
+    while(current != NULL){
+        forward = current -> next;
+        current -> next = previous;
+        previous = current;
+        current = forward;
+    }
+    return previous;
+}
+}
